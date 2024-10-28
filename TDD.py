b600aee41e57d28da2a7e11447a07666b330b26e
Adam <adamos139@wp.pl>
2024-10-28 14:57:29 +0100

    TDD.py improved

--- TDD.py
+++ TDD.py
@@ -5,7 +5,7 @@ class PaymentProcessor:
     def __init__(self, gateway: PaymentGateway):
         self.gateway = gateway
     
-    def processPayment(userId, amount):
+    def processPayment(userId: str, amount: float) -> TransactionResult:
         try:
             if amount < 0:
                 raise PaymentException("Podano ujemną kwotę.")
@@ -23,7 +23,7 @@ class PaymentProcessor:
             logging.error(f"Payment processing error: {str(e)}")
             return TransactionResult(False, "")
     
-    def refundPayment(transactionId):
+    def refundPayment(transactionId: str) -> TransactionResult:
     	if transactionId is None:
     	    raise NetworkException("Nie podano numeru transakcji")
     	try:
@@ -37,7 +37,7 @@ class PaymentProcessor:
             logging.error(f"Refund processing error: {str(e)}")
             return TransactionResult(False, "")
     	
-    def getPaymentStatus(transactionId):
+    def getPaymentStatus(transactionId: str) -> TransactionStatus:
         if not transactionId:
             raise NetworkException("Nie podano numeru transakcji")
         try:
@@ -49,22 +49,21 @@ class PaymentProcessor:
         
 #Klasa interfejs
 class PaymentGateway:
-    def charge(userId, amount):
+    def charge(userId: str, amount: float) -> TransactionResult:
         pass
         
-    def refund(transactionId):
+    def refund(transactionId: str) -> TransactionResult:
     	pass
     	
-    def getStatus(transactionId):
+    def getStatus(transactionId: str) -> TransactionStatus:
     	pass
  
     	
 class TransactionResult:
-    def __init__(self, success, transactionId, message, TransactionStatus):
+    def __init__(self, success: bool, transactionId: str, message: str = ""):
     	self.success = success
     	self.transactionId = transactionId
     	self.message = message
-    	self.TransactionStatus = TransactionStatus
  
    
 class TransactionStatus(Enum):
